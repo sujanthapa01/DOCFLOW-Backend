b80b115034d70f70c30c2b9e1826ed002bd7d07f
Sujan Thapa <sujanthapa4001@gmail.com>
2026-01-05 20:55:59 +0530

    feat(aichat): add aiChat controller with feature stream/non-stream

--- controllers/aichat.controller.ts
+++ controllers/aichat.controller.ts
@@ -1,14 +1,52 @@
 import type { Request, Response } from "express"
+import { OpenRouterChatService } from "../services/OpenRouterChatService"
+import { ChatMeggage } from "../types/chat"
 
-export const aiChatController = (req: Request,res:Response) => {
-    
-    const {prompt} = req.body
-    try{
-        
+// Chat Service Instence
+const chatService = new OpenRouterChatService()
+
+export const aiChatController = async (req: Request, res: Response) => {
+
+    const { message, stream, model } = req.body as {
+        message: ChatMeggage[],
+        stream?: boolean,
+        model?: string
+    }
+
+    if (!message || Array.isArray(message)) {
+        res.json({ msg: "messssge is required" })
+    }
+
+
+    if (stream) {
+        res.setHeader("Content-type", "text/event-stream ")
+        res.setHeader("Cache-controll", "no-cache")
+        res.setHeader("Connection", "keep-alive")
+
+        const streamResponse = await chatService.chat(message, {
+            model: model,
+            stream: true
+        })
+
+        for await (const token of streamResponse as AsyncIterable<string>) {
+            res.write(`$data : ${token} \n\n`)
+        }
+
+        return res.end()
+
+    }
+
+    try {
+
+        const res = await chatService.chat(message, {
+            stream: stream,
+            model: model
+        })
 
 
 
-    }catch(error){
+
+    } catch (error) {
 
         throw new Error("internal server error ''aiChat COntroller''")
 
